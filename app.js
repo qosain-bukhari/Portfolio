6b1d0ba59515d0fcd99c07b965d09c78d591d26e
qosain-bukhari <bukhariqosain824@gmail.com>
2025-07-15 01:03:52 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+document.addEventListener("DOMContentLoaded", function () {
+    const downloadBtn = document.getElementById("download-cv");
+
+    downloadBtn.addEventListener("click", function (e) {
+        const link = document.createElement("a");
+        link.href = "Qosain_Bukhari_CV.pdf";  // Update the path if the file is in a folder
+        link.download = "Qosain_Bukhari_CV.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+});
